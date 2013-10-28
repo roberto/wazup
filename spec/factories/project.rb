7bf9cab91acfe382089cdeb39b680f4d89ccdb83
Roberto Soares <roberto.tech@gmail.com>
2013-10-28 17:42:35 -0200

    using factory girl on project specs

---
+++ spec/factories/project.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :project do
+    title 'Project 123'
+  end
+end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,13 +1,26 @@
 require 'spec_helper'
 
 describe Project do
-  it "is invalid without a title" do
-    subject.title = nil
-    expect(subject).to_not be_valid
+  subject do
+    create(:project)
   end
 
-  it "is invalid with an empty title" do
-    subject.title = "  "
-    expect(subject).to_not be_valid
+  context "is invalid" do
+    it "without a title" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "with an empty title" do
+      subject.title = "  "
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context "is valid" do
+    it "with a title" do
+      subject.title = "Project 1"
+      expect(subject).to be_valid
+    end
   end
 end
