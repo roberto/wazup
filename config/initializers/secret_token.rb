a1bedd4d8de9fe43f4e7e7869fa9c232c2cd97be
Roberto Soares <roberto.tech@gmail.com>
2013-10-28 15:30:04 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wazup::Application.config.secret_token = 'b03e032682e9f8320a153ee805cbc1ea8d7c81a00ea7a50b9f873babbfe72d861e797e1393e359996fa92bc8728427a99d38430e7282c5cccd4d3edf8b6e1e69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wazup::Application.config.session_store :cookie_store, key: '_wazup_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wazup::Application.config.session_store :active_record_store
