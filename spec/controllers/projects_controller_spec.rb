58eddef1a3589685681027d537919f558d6ae804
Roberto Soares <roberto.tech@gmail.com>
2013-10-29 08:48:23 -0200

    projects: adding pending specs

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -11,7 +11,7 @@ describe ProjectsController do
         post 'create', project: project.as_json
       end
 
-      it "returns http success" do
+      it "returns http redirect" do
         send_data
         expect(response).to be_redirect
       end
@@ -20,6 +20,27 @@ describe ProjectsController do
         expect do 
           send_data
         end.to change{Project.count}.by(1)
+      end
+
+      it "redirects to dashboard page"
+    end
+
+    context "invalid project" do
+      it "returns http invalid data"
+      it "doesn't save project"
+    end
+  end
+
+  describe "GET 'show'" do
+    describe "format js" do
+      context "project has been found" do
+        it "returns http success"
+        it "renders project area"
+      end
+
+      context "project hasn't been found" do
+        it "returns http error"
+        it "renders project area"
       end
     end
   end
